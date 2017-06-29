8992dcb0aa2547916f36f65ec413617a043457ff
sheychen <contact@wadza.fr>
2017-06-29 16:40:39 +0200

    Move to Nextcloud

--- App/encryption/start-hidden.bat
+++ App/encryption/start-hidden.bat
@@ -3,4 +3,4 @@
 :: Get the password
 powershell -Command $pword = read-host "Enter password" -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp.txt & set /p passwd=<.tmp.txt & del .tmp.txt
 
-wscript.exe hidden.vbs ownCloudPortableEncryptionHelper.exe %passwd% %1
+wscript.exe hidden.vbs nextcloudPortableEncryptionHelper.exe %passwd% %1
